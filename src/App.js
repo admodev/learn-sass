f84b080c408853f97a9cf567e2cd201e1624e9c2
admodev <admosdigital@yahoo.com>
2021-04-30 17:37:03 -0300

    learn sass

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="primerDiv">
+        <h1>Hola, Mundo!</h1>
+      </div>
+
+      <ul>
+        <li>
+          <a href="#">home</a>
+        </li>
+        <li>
+          <a href="#">about</a>
+        </li>
+        <li>
+          <a href="#">exit</a>
+        </li>
+      </ul>
+
+      <h2>Título</h2>
+
+      <p>
+        añlskdnjkas ldjhasdl jahsdkaj sdkalsjhd alksjdhaskjd aklsjdh askljdha
+      </p>
+
+      <h1>Aprendiste SASS en 15 minutos!</h1>
+
+      <div className="container">
+        <section></section>
+        <aside></aside>
+      </div>
+    </>
   );
 }
 
